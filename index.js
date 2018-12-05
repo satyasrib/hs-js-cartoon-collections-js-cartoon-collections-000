b804712170307876fbf143790fb773aac7b6af1f
Satyasri Varaganti <satyasrib@gmail.com>
2018-12-05 20:31:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+var newdwarves = [];
+for (let i = 0; i< dwarves.length; i++) {
+  newdwarves.push(i+1 +"."+ dwarves[i]);
+}
+return dwarves
 }
 
 function summonCaptainPlanet(planeteerCalls){
