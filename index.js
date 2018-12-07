04f892e71e2fa2db42edda680dc6c548f8b31647
Satyasri Varaganti <satyasrib@gmail.com>
2018-12-07 06:19:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+
 function dwarfRollCall(dwarves) {
+var newdwarves = []
+for (let i = 0; i< dwarves.length; i++) {
+  newdwarves.push(i+1 +". "+ dwarves[i]);
+}
+return (newdwarves.join(" ") + " ")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+var newarray = [];
+for (let i = 0; i< planeteerCalls.length; i++) {
+  newarray.push(planeteerCalls[i].toUpperCase() + "!");
+}
+return newarray
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length > 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+var cheese = ["cheddar", "gouda", "camembert"];
+  for (var i = 0; i< foods.length; i++) {
+    for (var j = 0; j< 3 ; j++) {
+      if (foods[i] === cheese[j])
+      return foods[i];
+    }
+  }
+return "no cheese!";
 }
